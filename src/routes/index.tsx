47283c7d2ee7323cb3fe67e966ed9b0882c03865
Germys G <megustaelmangu47@gmail.com>
2023-06-01 12:52:30 -0400

    Aff

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,27 +1,38 @@
 import {
+  $,
   component$,
-  useStore,
+  useOnWindow,
+  useSignal,
   useStylesScoped$,
-  useVisibleTask$,
 } from '@builder.io/qwik';
 import type { DocumentHead } from '@builder.io/qwik-city';
 import styles from './index.css?inline';
 
 export default component$(() => {
   useStylesScoped$(styles);
-  const store = useStore({
-    count: 0,
-  });
 
-  useVisibleTask$(() => {
-    // Only runs in the client
-    const timer = setInterval(() => {
-      store.count++;
-    }, 2);
-    return () => {
-      clearInterval(timer);
-    };
-  });
+  const windowWidth = useSignal(1800);
+  const videoSrc = useSignal(
+    'https://res.cloudinary.com/douvery/video/upload/v1685621641/ifr5yndpanqlrptc1gwr.mp4'
+  );
+
+  useOnWindow(
+    'load',
+    $(() => {
+      windowWidth.value = window.innerWidth;
+    })
+  );
+
+  useOnWindow(
+    'resize',
+    $(() => {
+      videoSrc.value =
+        windowWidth.value > 1400
+          ? 'https://res.cloudinary.com/douvery/video/upload/v1685621641/ifr5yndpanqlrptc1gwr.mp4'
+          : 'https://res.cloudinary.com/douvery/video/upload/v1685637514/xlxzh6aln0o8jaylrolh.mp4';
+    })
+  );
+
   return (
     <div class="container-all">
       <div class="carousel__image-container">
@@ -29,7 +40,7 @@ export default component$(() => {
           width={1800}
           height={350}
           class="carousel__image"
-          src="https://res.cloudinary.com/douvery/video/upload/v1685621641/ifr5yndpanqlrptc1gwr.mp4"
+          src={videoSrc.value}
           autoPlay
           muted
         />
