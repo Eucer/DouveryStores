93690d6f50d60129cc100333e3618bd52ccdcfca
Eucer <megustaelmangu47@gmail.com>
2023-06-03 18:26:04 -0400

    AGG

--- src/routes/(AUTH)/auth/register/index.tsx
+++ src/routes/(AUTH)/auth/register/index.tsx
@@ -2,6 +2,7 @@ import { $, component$, useSignal, useStylesScoped$ } from '@builder.io/qwik';
 import { type DocumentHead } from '@builder.io/qwik-city';
 import style from './index.css?inline';
 import { DouveryAuthLogo } from '~/components/DouveryAuthLogo/douvery-auth-logo';
+import { TermsConditions } from '~/components/Terms&Conditions/terms-Conditions';
 export default component$(() => {
     useStylesScoped$(style);
     const step = useSignal(1);
@@ -28,16 +29,19 @@ export default component$(() => {
 
                 {step.value === 1 && <PersonalInfo nextStep={nextStep} />}
                 {step.value === 2 && <StoreInfo nextStep={nextStep} prevStep={prevStep} />}
+
                 {step.value === 3 && <ShippingInfo prevStep={prevStep} />}
                 <br />
                 <div class="hr1" />
                 <br />
+
                 <div class="form-group need-account">
-                    ¿No tiene una cuenta?
-                    <a href="/a/register" class="forgot-new-account-link">
-                        Crear una
+                    ¿Ya tiene una cuenta?
+                    <a href="/auth/login" class="forgot-new-account-link">
+                        Iniciar sesión
                     </a>
                 </div>
+                <TermsConditions />
             </div>
 
         </div>
@@ -48,24 +52,27 @@ const PersonalInfo = ({ nextStep }: any) => (
     <form class="form-container">
         {/* Sección 1: Información Personal */}
         <div class="info-section">
-            <div class="section-title">Información Personal</div>
-
             <br />
-
             <div class="form-group">
-                <label for="name">Nombre:</label>
+                <label for="name">Nombre de Administrador</label>
                 <input type="text" id="name" name="name" required autoComplete="on" />
             </div>
             <div class="form-group">
-                <label for="lastName">Apellido:</label>
-                <input type="text" id="lastName" name="lastName" required autoComplete="on" />
-            </div>
-            <div class="form-group">
-                <label for="email">Email de preferencia</label>
+                <label for="email">Email de Administrador</label>
                 <input type="email" id="email" name="email" required />
             </div>
             <div class="form-group">
-                <label for="password">Contraseña:</label>
+                <label for="email">Phone de Administrador</label>
+                <input type="tel" id="phone" name="phone" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" required />
+                <small>Formato: 123-456-7890</small>
+
+            </div>
+            <div class="form-group">
+                <label for="password">Contraseña</label>
+                <input type="password" id="password" name="password" required autoComplete="on" />
+            </div>
+            <div class="form-group">
+                <label for="password">Confirmar Contraseña</label>
                 <input type="password" id="password" name="password" required autoComplete="on" />
             </div>
         </div> <br />
@@ -82,20 +89,32 @@ const StoreInfo = ({ nextStep, prevStep }: any) => (
     <form class="form-container">
         {/* Sección 2: Información de la Tienda */}
         <div class="info-section">
-            <div class="section-title">Información de la Tienda</div>
+
             <br />
             <div class="form-group">
                 <label for="storeName">Nombre de la Tienda</label>
                 <input type="text" id="storeName" name="storeName" required />
             </div>
-            <div class="form-group">
-                <label for="storeCategory">Categoría de la Tienda</label>
-                <input type="text" id="storeCategory" name="storeCategory" required />
-            </div>
+
             <div class="form-group">
                 <label for="storeDescription">Descripción de la Tienda</label>
                 <textarea id="storeDescription" name="storeDescription" required></textarea>
             </div>
+            <div class="form-group">
+                <label for="email">Email contacto</label>
+                <input type="email" id="email" name="email" required />
+            </div>
+            <div class="form-group">
+                <label for="email">Tipo de tienda</label>
+                <select id="elegant-select">
+                    <option  >Tienda multiproducto</option>
+
+                </select>
+
+            </div>
+
+
+
         </div> <br />
         <div class="buttons-container">
             <button type="button" class="prev-button" onClick$={prevStep}>
@@ -112,12 +131,22 @@ const ShippingInfo = ({ prevStep }: any) => (
     <form class="form-container">
         {/* Sección 3: Información de Envío */}
         <div class="info-section">
-            <div class="section-title">Información de Envío</div>
+
             <br />
             <div class="form-group">
-                <label for="shippingLocation">Ubicación del Envío</label>
+                <label for="email">Pais origen</label>
+                <select id="elegant-select">
+                    <option   >Dominican Republic</option>
+                    <option   >United States</option>
+
+                </select>
+
+            </div>
+            <div class="form-group">
+                <label for="shippingLocation">Ubicación de establecimiento</label>
                 <input type="text" id="shippingLocation" name="shippingLocation" required />
             </div>
+
         </div>
         <br />
         <div class="buttons-container">
